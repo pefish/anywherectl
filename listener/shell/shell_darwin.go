3f0454ea533eb7667a9b30880640aff0486e1bc6
pefish <pefish@qq.com>
2020-04-27 20:18:00 +0800

    Shell command stream

--- listener/shell/shell_darwin.go
+++ listener/shell/shell_darwin.go
@@ -3,24 +3,9 @@
 package shell
 
 import (
-	"bufio"
-	"fmt"
 	"os/exec"
 )
 
 func GetCmd(s string) (*exec.Cmd, error) {
 	return exec.Command("/bin/bash", "-c", s), nil
-}
-
-
-func ExecShellStream(s string) {
-	cmd := exec.Command("/bin/bash", "-c", s)
-	reader_, _ := cmd.StdoutPipe()
-	cmd.Start()
-	reader := bufio.NewReader(reader_)
-	for {
-		line, _, _ := reader.ReadLine()
-		fmt.Println(string(line))
-	}
-	cmd.Wait()
 }
